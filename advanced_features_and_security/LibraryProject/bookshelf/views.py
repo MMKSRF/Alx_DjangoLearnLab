96807850e28d1c3ef397f1e54d722cb8ad3d7b33
MMKSRF <perezendale@gmail.com>
2025-11-23 04:00:22 -0800

    dddddsfsdgfgfdgd

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
 from .forms import BookForm
+from .forms import ExampleForm   # <-- Added as required
 
 
 # Permission-protected views
