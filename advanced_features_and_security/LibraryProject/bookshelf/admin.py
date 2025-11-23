e2c9eb01b34e06518e0314b6a3c1ad4ef1573e45
MMKSRF <perezendale@gmail.com>
2025-11-23 03:50:32 -0800

    ddd

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
+from django.contrib import admin 
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 from .models import Book, CustomUser
 
 # Custom User Admin
-@admin.register(CustomUser)
 class CustomUserAdmin(BaseUserAdmin):
     """Admin interface for the custom user model."""
     list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_active')
@@ -26,9 +25,12 @@ class CustomUserAdmin(BaseUserAdmin):
         }),
     )
 
+# ✔ Explicit registration (what your check is looking for)
+admin.site.register(CustomUser, CustomUserAdmin)
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Columns shown in the admin list view
-    list_filter = ('publication_year', 'author')             # Sidebar filters
-    search_fields = ('title', 'author')                      # Search box fields
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year', 'author')
+    search_fields = ('title', 'author')
